611ef17c63b9f803f0535a8a8eaba75cdb3d0344
Juan N <cristhiann017@gmail.com>
2024-10-24 18:41:36 -0500

    feature(backend): crud for categories implemented

--- src/main/java/com/juan/api/AdminCategoryController.java
+++ src/main/java/com/juan/api/AdminCategoryController.java
@@ -1,8 +1,73 @@
 package com.juan.api;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.juan.model.entity.Category;
+import com.juan.repository.service.AdminCategoryService;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/admin/categories")
 public class AdminCategoryController {
 
-    void hello(){
-        System.err.println("Hello from category controller");
+    private final AdminCategoryService adminCategoryService;
+
+    // public AdminCategoryController(AdminCategoryService adminCategoryService) {
+    //     this.adminCategoryService = adminCategoryService;
+    // }
+
+    @GetMapping
+    public ResponseEntity<List<Category>> getAllCategories() {
+        List<Category> categories = adminCategoryService.getAll();
+        return new ResponseEntity<List<Category>>(categories, HttpStatus.OK);
     }
+
+    @GetMapping("/page")
+    public ResponseEntity<Page<Category>> paginateCategories(
+            @PageableDefault(size = 5, sort = "name") Pageable pageable) {
+        Page<Category> categories = adminCategoryService.paginate(pageable);
+        return new ResponseEntity<Page<Category>>(categories, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Category> createCategory(@RequestBody Category category) {
+        Category newCategory = adminCategoryService.create(category);
+        return new ResponseEntity<Category>(newCategory, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Category> getCategoryById(@PathVariable("id") Integer id) {
+        Category category = adminCategoryService.finById(id);
+        return new ResponseEntity<Category>(category, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Category> updateCategory(@PathVariable("id") Integer id, @RequestBody Category category) {
+        Category updatedCategory = adminCategoryService.update(id, category);
+        return new ResponseEntity<Category>(updatedCategory, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Category> deleteCategory(@PathVariable("id") Integer id) {
+        adminCategoryService.delete(id);
+        return new ResponseEntity<Category>(HttpStatus.NO_CONTENT);
+    }
+
 }
--- src/main/java/com/juan/repository/service/Impl/AdminCategoryServiceImpl.java
+++ src/main/java/com/juan/repository/service/Impl/AdminCategoryServiceImpl.java
@@ -1,8 +1,66 @@
 package com.juan.repository.service.Impl;
 
-public class AdminCategoryServiceImpl {
-    void hello(){
-        System.err.println("Hello from service impl");
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.juan.model.entity.Category;
+import com.juan.repository.CategoryRepository;
+import com.juan.repository.service.AdminCategoryService;
+
+@Service
+public class AdminCategoryServiceImpl implements AdminCategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<Category> getAll() {
+        return categoryRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public Page<Category> paginate(Pageable pageable) {
+        return categoryRepository.findAll(pageable);
+    }
+
+    @Transactional
+    @Override
+    public Category create(Category category) {
+        category.setCreatedAt(LocalDateTime.now());
+        return categoryRepository.save(category);
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public Category finById(Integer id) {
+        return categoryRepository.findById(id).orElseThrow(() -> new RuntimeException("Category Not Found"));
+    }
+
+    @Transactional
+    @Override
+    public Category update(Integer id, Category updateCategory) {
+        Category categoryFromDB = finById(id);
+
+        categoryFromDB.setName(updateCategory.getName());
+        categoryFromDB.setDescription(updateCategory.getDescription());
+        categoryFromDB.setUpdatedAt(LocalDateTime.now());
+
+        return categoryRepository.save(categoryFromDB);
+    }
+
+    @Transactional
+    @Override
+    public void delete(Integer id) {
+        Category categoryFromDB = finById(id);
+        categoryRepository.delete(categoryFromDB);
     }
 
 }
